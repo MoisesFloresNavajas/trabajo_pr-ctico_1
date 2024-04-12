d73a4831965b03bfaac5da3097c76cea28e2b8a9
MoisesFloresNavajas <moisesfloresnavajas@gmail.com>
2024-04-12 18:59:32 -0300

    Resolución ejercicio 2

---
+++ tp1_FloresNavajasMoisés/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,32 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nPais;
+		int edadPersona;
+		double hEdificio, precioP, coseno = 0.5;
+		String numTelefono;
+		
+		nPais = "Argentina";
+		System.out.println("Nombre del país: " + nPais);
+        
+        edadPersona = 28;
+        System.out.println("Edad de la persona: " + edadPersona);
+        
+        
+        hEdificio = 8.5;
+        System.out.println("La altura del edificio es: " + hEdificio);
+        
+        precioP = 232345.6;
+        System.out.println("El precio del producto del supermercado es: " + precioP);
+        
+        numTelefono = "777777777";
+        System.out.println("Número de teléfono: " + numTelefono);
+        
+        Math.cos(coseno);
+        System.out.println("El coseno de 0.5 es = " + coseno);
+	}
+
+}
