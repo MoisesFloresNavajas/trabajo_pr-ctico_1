ebf2003dc9b7940a192972b93a0433068e85aea5
MoisesFloresNavajas <moisesfloresnavajas@gmail.com>
2024-04-12 18:34:11 -0300

    Resolución ejercicio 1

--- tp1_FloresNavajasMoisés/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_FloresNavajasMoisés/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Bienvenido a Programación Visual!!");
-	}
+		int vr1 =9, vr2=5, vr3=34, vr4=7, vr5=8;
+		float valorTotal, promedio;
+
+		valorTotal = vr1 + vr2 + vr3 + vr4 + vr5;
+		
+		promedio = valorTotal / 5;
+		
+		System.out.println("El promedio es: " + promedio);
+		
+	}		
 
 }
